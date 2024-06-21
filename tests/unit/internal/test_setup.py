8c8f3c22061685e3f466f28cd70bc28ccbec4952
Maciej Urbanski <maciej.rooter.urbanski@reef.pl>
2024-06-21 19:30:54 +0200

    cruft update (fix lint error in test_setup)

--- tests/unit/internal/test_setup.py
+++ tests/unit/internal/test_setup.py
@@ -1,4 +1,6 @@
 """
+Test setup tests.
+
 This test file is here always to indicate that everything was installed and the CI was able to run tests.
 It should always pass as long as all dependencies are properly installed.
 """
