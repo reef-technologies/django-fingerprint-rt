74ab2f99b43cb23a8d3f62c430cd1ef2f3b98c08
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2026-02-12 15:10:04 +0300

    Enable redis in CI

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from __future__ import annotations
 
 from datetime import timedelta
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
